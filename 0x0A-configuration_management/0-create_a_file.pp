71323b69e752620c3498fbaf49427090f65c30c7
oran2527 <orlago250183@gmail.com>
2020-03-23 21:26:33 +0000

    point 0 lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,8 +3,9 @@
 file { 'tmp/holberton':
     ensure  => file
     path    => '/tmp/holberton',
-    content => 'I love Puppet',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744',
+    content => 'I love Puppet',
   }
