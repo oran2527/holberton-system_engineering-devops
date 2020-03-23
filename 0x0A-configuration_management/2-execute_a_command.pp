263d617420744ae7ccac49900112abe28b6d3304
oran2527 <orlago250183@gmail.com>
2020-03-23 21:09:05 +0000

    point 2 trailing

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -4,4 +4,4 @@ exec { 'pkill':
 	command   => 'pkill killmenow',
 	path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 	provider  => 'shell',
-}	
+}
