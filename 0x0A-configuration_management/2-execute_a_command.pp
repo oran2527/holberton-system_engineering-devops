432db0406ba04a57d68fec473771a17fbadc7bae
oran2527 <orlago250183@gmail.com>
2020-03-23 21:14:44 +0000

    point 2 lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Manifest that executes and kill process
 #!/usr/bin/env bash
 exec { 'pkill':
-	command   => 'pkill killmenow',
-	path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-	provider  => 'shell',
+command  => 'pkill killmenow',
+path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+provider => 'shell',
 }
