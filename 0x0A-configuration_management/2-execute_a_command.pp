8618c269e4ea5727a1d570f3b2a6e507ea7362a9
oran2527 <orlago250183@gmail.com>
2020-03-23 21:11:03 +0000

    point 2 order comment

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
 # Manifest that executes and kill process
+#!/usr/bin/env bash
 exec { 'pkill':
 	command   => 'pkill killmenow',
 	path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
