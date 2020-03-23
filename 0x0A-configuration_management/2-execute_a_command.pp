34e18184a2806fb25d63f94fdfd19eae5cf22884
oran2527 <orlago250183@gmail.com>
2020-03-23 21:07:13 +0000

    point 2 kill a process

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Manifest that executes and kill process
+exec { 'pkill':
+	command   => 'pkill killmenow',
+	path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+	provider  => 'shell',
+}	
