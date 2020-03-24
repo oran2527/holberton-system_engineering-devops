4529e8e127304ef4bbbe953bd00d9f379d6e35d1
oran2527 <orlago250183@gmail.com>
2020-03-24 18:06:53 +0000

    point 4 puppet erase ~

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 include stdlib
 
 file_line { 'IdentityFile':
-        path => '~/etc/ssh/ssh_config',
+        path => '/etc/ssh/ssh_config',
         line => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'PasswordAuthentication':
-        path => '~/etc/ssh/ssh_config',
+        path => '/etc/ssh/ssh_config',
         line => 'PasswordAuthentication no',
 }
