ac7b0a3638b4c231da26392539f540b3bf686da3
oran2527 <orlago250183@gmail.com>
2020-03-24 15:03:31 +0000

    point 0 fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # This manifest creates a file with some specifications
-#!/usr/bin/env bash
-file { 'tmp/holberton':
-    ensure  => file
-    path    => '/tmp/holberton',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet'
-  }
+file { '/tmp/holberton':
+        ensure  => file,
+        path    => '/tmp/holberton',
+        mode    => '0744',
+        owner   => 'www-data',
+        group   => 'www-data',
+        content => 'I love Puppet'
+}
