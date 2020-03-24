b5b11d961bccd4da434c737fa2f07a6df3e8cf4f
oran2527 <orlago250183@gmail.com>
2020-03-24 17:53:48 +0000

    point 4 puppet

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# configuration with puppet
+file_line { 'PasswordAuthentication':
+        path => '~/etc/ssh/ssh_config',
+        line => 'PasswordAuthentication=no',
+}
+file_line { 'IdentityFile':
+        path => '~/etc/ssh/ssh_config',
+        line => 'IdentityFile=~/.ssh/holberton'
+}
