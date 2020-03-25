905d7e3467ea4c3746812a0daec53af830aa4ff9
oran2527 <orlago250183@gmail.com>
2020-03-25 19:28:32 +0000

    point 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
-# configuration with puppet
+# private key ~/.ssh/holberton refuse authentication
 include stdlib
 
-file_line { 'IdentityFile':
-        path => '/etc/ssh/ssh_config',
-        line => 'IdentityFile ~/.ssh/holberton',
+file_line { 'Use private key':
+          path => '/etc/ssh/ssh_config',
+          line => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line { 'PasswordAuthentication':
-        path => '/etc/ssh/ssh_config',
-        line => 'PasswordAuthentication no',
+file_line { 'Refuse password':
+          path => '/etc/ssh/ssh_config',
+          line => 'PasswordAuthentication no',
 }
